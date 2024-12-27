d2e0e0b30a912de8648481ef26e3897c21d1599f
Gerard <gesteve.12@gmail.com>
2024-12-27 15:02:56 +0100

    Install prettier

--- components/ui/CarouselCard.tsx
+++ components/ui/CarouselCard.tsx
@@ -1,89 +1,104 @@
-import { View, Text, StyleSheet, TextInput, Button, Alert, FlatList } from "react-native"
-import { useEffect, useState } from "react"
-import { supabase } from "@/lib/supabase"
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  Alert,
+  FlatList,
+} from 'react-native'
+import { useEffect, useState } from 'react'
+import { supabase } from '@/lib/supabase'
 
 export const CarouselCard = ({ info }: any) => {
-    const [productOpinion, setProductOpinion] = useState<string>('')
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    const [opinions, setOpinions] = useState<any>([]);
+  const [productOpinion, setProductOpinion] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [opinions, setOpinions] = useState<any>([])
 
-    useEffect(() => {
-        const fetchData = async () => {
-        let { data: products, error } = await supabase
-            .from('products')
-            .select("*")
-            .eq('barcode', info.barcode);
-        
-            console.log({ products });
+  useEffect(() => {
+    const fetchData = async () => {
+      let { data: products, error } = await supabase
+        .from('products')
+        .select('*')
+        .eq('barcode', info.barcode)
 
-        let { data: opinions, error: opinionsError } = await supabase
-            .from('opinions')
-            .select("*")
-            .eq('product', info.barcode);
-        
-        if (opinionsError) {
-            Alert.alert('Error', opinionsError.message);
-        } else {
-            setOpinions(opinions);
-        }
-        };
+      console.log({ products })
 
-        fetchData();
-    }, [info.barcode]);
-    
-    const postNewProduct = async () => {
-        try { 
-            const { data, error } = await supabase
-            .from('products')
-            .upsert([
-                { barcode: info.barcode },
-            ]).select()
-            
-            if (error) {
-                throw error;
-            }
-        } catch(error:any) {
-            Alert.alert('Error', error.message)
-        } finally {
-            setIsLoading(false)
-        }
+      let { data: opinions, error: opinionsError } = await supabase
+        .from('opinions')
+        .select('*')
+        .eq('product', info.barcode)
+
+      if (opinionsError) {
+        Alert.alert('Error', opinionsError.message)
+      } else {
+        setOpinions(opinions)
+      }
     }
 
-    const submitProductOpinion = async () => {
-        if (!productOpinion.trim()) {
-            Alert.alert('Error', 'Please enter an opinion')
-            return
-        }
-        setIsLoading(true)
-        
-        postNewProduct()
+    fetchData()
+  }, [info.barcode])
 
-        try { 
-            const { data, error } = await supabase
-            .from('opinions')
-            .insert([
-                { product: info.barcode, opinion: productOpinion, user_id: 1 },
-            ]).select()
-            
-            if (error) {
-                throw error;
-            }
+  const postNewProduct = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('products')
+        .upsert([{ barcode: info.barcode }])
+        .select()
 
-            setOpinions([...opinions, ...data]);
-        } catch(error:any) {
-            Alert.alert('Error', error.message)
-        } finally {
-            setIsLoading(false)
-        }
+      if (error) {
+        throw error
+      }
+    } catch (error: any) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsLoading(false)
     }
+  }
 
-    return (
-        <View style={styles.slideContent}>
-            <Text>{info.barcode}</Text>
-            <TextInput value={productOpinion} onChangeText={setProductOpinion}></TextInput>
+  const submitProductOpinion = async () => {
+    if (!productOpinion.trim()) {
+      Alert.alert('Error', 'Please enter an opinion')
+      return
+    }
+    setIsLoading(true)
 
-            <Button title={isLoading ? 'Saving...' : 'Save'} onPress={submitProductOpinion} disabled={isLoading}></Button>
-            <FlatList
+    postNewProduct()
+
+    try {
+      const { data, error } = await supabase
+        .from('opinions')
+        .insert([
+          { product: info.barcode, opinion: productOpinion, user_id: 1 },
+        ])
+        .select()
+
+      if (error) {
+        throw error
+      }
+
+      setOpinions([...opinions, ...data])
+    } catch (error: any) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <View style={styles.slideContent}>
+      <Text>{info.barcode}</Text>
+      <TextInput
+        value={productOpinion}
+        onChangeText={setProductOpinion}
+      ></TextInput>
+
+      <Button
+        title={isLoading ? 'Saving...' : 'Save'}
+        onPress={submitProductOpinion}
+        disabled={isLoading}
+      ></Button>
+      <FlatList
         data={opinions}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
@@ -92,21 +107,21 @@ export const CarouselCard = ({ info }: any) => {
           </View>
         )}
       />
-        </View>
-    )
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    slideContent: {
-        height: '100%',
-        width: '100%',
-        borderRadius: 15,
-        padding: 10,
-        backgroundColor: 'white'
-    },
-    opinionItem: {
+  slideContent: {
+    height: '100%',
+    width: '100%',
+    borderRadius: 15,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  opinionItem: {
     padding: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
-  }
+    borderBottomColor: '#ccc',
+  },
 })
