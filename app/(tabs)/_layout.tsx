673066068c61d5f80cb0f021ae1c0642db83f139
Gerard <gesteve.12@gmail.com>
2024-12-27 16:42:43 +0100

    more cleaning

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,19 +3,12 @@ import React from 'react'
 import { Platform } from 'react-native'
 
 import { IconSymbol } from '@/components/ui/IconSymbol'
-import TabBarBackground from '@/components/ui/TabBarBackground'
-import { Colors } from '@/constants/Colors'
-import { useColorScheme } from '@/hooks/useColorScheme'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme()
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
