fb7ab70186b1a39cc4e4f23a5959c4128b11b01c
Gerard <gesteve.12@gmail.com>
2024-12-30 01:22:54 +0100

    add google auth

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,20 +1,24 @@
-import { StyleSheet, Text, Image, Platform, View } from 'react-native';
+import { StyleSheet, Text, Image, Platform, View } from 'react-native'
+import GoogleSign from '@/components/auth.native'
 
 export default function TabTwoScreen() {
-  return (
-    <View><Text>Holaaa</Text></View>
-  );
+    return (
+        <View>
+            <Text>Holaaa</Text>
+            <GoogleSign></GoogleSign>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-});
+    headerImage: {
+        color: '#808080',
+        bottom: -90,
+        left: -35,
+        position: 'absolute',
+    },
+    titleContainer: {
+        flexDirection: 'row',
+        gap: 8,
+    },
+})
