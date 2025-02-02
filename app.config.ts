a4315452ab3fc035d819a327d483fd909c633208
Gerard <gesteve.12@gmail.com>
2025-02-03 00:40:54 +0100

    prepare builds

--- app.config.ts
+++ app.config.ts
@@ -31,7 +31,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     ...config,
     name: getAppName(),
     slug: 'kali',
-    version: '1.1.0',
+    version: '0.2.1',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'myapp',
