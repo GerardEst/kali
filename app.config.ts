dbecb26b84db7209e8ff6532cd5542b3a71042ec
Gerard Esteve <gesteve.12@gmail.com>
2025-02-17 23:10:17 +0100

    new version

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 import { ConfigContext, ExpoConfig } from 'expo/config'
 
-const APP_VERSION = '0.3.1'
+const APP_VERSION = '0.3.2'
 
 const IS_DEV = process.env.APP_VARIANT === 'development'
 const IS_PREVIEW = process.env.APP_VARIANT === 'preview'
