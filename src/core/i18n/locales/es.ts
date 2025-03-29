e952c337913478b004759c5920954bddc7ed4d85
gerard.esteve <gesteve.12@gmail.com>
2025-03-29 10:25:31 +0100

    esp

--- src/core/i18n/locales/es.ts
+++ src/core/i18n/locales/es.ts
@@ -1,12 +1,20 @@
 export default {
     reviews_ownReview: 'Tu opinión',
     reviews_otherReviews: 'Opinión general',
-    product_addNote: 'Añadir nota',
+    product_addNote: 'Crear una nota',
     product_editNote: 'Editar nota',
+    product_addReview: 'Valorar',
+    product_editReview: 'Modificar',
     product_deleteNote: 'Eliminar',
     product_open: 'Abrir',
-    caroussel_addNote:
-        'Añade una nota al producto y la verás siempre que lo escanees, o en tu lista de notas',
+    reviews_nutritionReview: 'Info. Nutricional',
+    nutrition_missing: 'No hay información nutricional disponible',
+    nutrition_popup_description:
+        'Información de Open Food Facts, una base de datos abierta y libre',
+    nutrition_novascore_missing: 'No hay información de Novascore',
+    nutrition_nutriscore_missing: 'No hay información de Nutriscore',
+    caroussel_addNote: 'Añadir una nota privada al producto',
+    caroussel_addReview: 'Valorar el producto y ayudar a otros',
     tabs_saved: 'Guardados',
     tabs_notes: 'Notas',
     tabs_search: 'Escáner',
@@ -15,6 +23,7 @@ export default {
     common_unknown: 'Desconocido',
     common_anonymous: 'Anónimo',
     common_loading: 'Cargando...',
+    common_close: 'Cerrar',
     settings_title: 'Configuración',
     settings_language: 'Idioma',
     settings_catalan: 'Català',
@@ -26,10 +35,15 @@ export default {
     scanner_cameraPermission_button: 'Dar permiso de cámara',
     notes_title: 'Tus notas privadas',
     notes_emptyState:
-        'Aun no has añadido ninguna nota a ningún producto... ¿A qué esperas?',
+        'Aún no has añadido ninguna nota a ningún producto. ¿A qué esperas?',
     saved_title: 'Productos guardados',
-    saved_emptyState: 'Aun no has guardado ningún producto... ¿A qué esperas?',
+    saved_emptyState: 'Aún no has guardado ningún producto. ¿A qué esperas?',
+    subscribe_call: 'Subscribete!',
     welcome_title: 'Bienvenido/a a La Compra',
     welcome_description:
         'Regístrate para empezar a escanear productos y compartir tus opiniones con la comunidad.',
+    scanner_firstTimeInstructions_1:
+        'Apunta con la cámara a cualquier código de barras para ver información, comentarios y tus notas privadas',
+    scanner_firstTimeInstructions_2:
+        '¡Bien! Los productos que escanees se guardarán aquí, mientras que arriba puedes ver tu opinión y la opinión general sobre el producto.',
 }
